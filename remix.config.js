c175ec817d530e67ffe3d4ecd8a0596061277902
Roger Stringer <roger@datamcfly.com>
2023-04-05 11:41:21 -0700

    testing

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,3 @@
-const {
-  createRoutesFromFolders,
-} = require("@remix-run/v1-route-convention");
-
-
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   //serverBuildTarget: "vercel",
@@ -17,13 +12,7 @@ module.exports = {
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
   future: {
-    v2_routeConvention: true,
-		v2_meta: true,
+		v2_meta: false,
     unstable_tailwind: true,
 	},
-  routes(defineRoutes) {
-    // uses the v1 convention, works in v1.15+ and v2
-    return createRoutesFromFolders(defineRoutes);
-  },
-
 };
